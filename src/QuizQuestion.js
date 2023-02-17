3b169e835d5ad3d70d4a6941b1153b725999018a
chamoon-code <118274935+chamoon-code@users.noreply.github.com>
2023-02-17 13:15:10 +0800

    2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,3 +1,16 @@
 import React, { Component } from 'react'
 
 let QuizQuestion extends Component
+
+<main>
+  <section>
+    <p>// instruction text goes here</p>
+  </section>
+  <section className="buttons">
+    <ul>
+      //quiz question button logic goes here
+    </ul>
+  </section>
+</main>
+
+export default QuizQuestion
