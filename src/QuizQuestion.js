0261e631d8ddf9c5e5277fb7a58a059a3e574190
chamoon-code <118274935+chamoon-code@users.noreply.github.com>
2023-02-17 13:22:06 +0800

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 
-let QuizQuestion extends Component
+let QuizQuestion extends Component{
+  constructor(props) {
+    super(props)
+    this.state = { quiz_position: 1 }
+  }
+  render(
+    <main>
+      <section>
+        <p>// instruction text goes here</p>
+      </section>
+      <section className="buttons">
+        <ul>
+          //quiz question button logic goes here
+        </ul>
+      </section>
+    </main>
 
-<main>
-  <section>
-    <p>// instruction text goes here</p>
-  </section>
-  <section className="buttons">
-    <ul>
-      //quiz question button logic goes here
-    </ul>
-  </section>
-</main>
+  ) {
+    return ()
+}
+
 
 export default QuizQuestion
